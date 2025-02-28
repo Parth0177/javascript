a872568b1e85188123bb1c371d4a09aeaa64ee34
Parth0177 <parthtiwari670@gmail.com>
2025-02-28 16:00:30 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"//treat all js code as newer version
+
+
+//alert() we are using nodejs not browser
+
+console.log(3+3)// code readablity should be good
+
+let name="Parth" // string
+let age=22  //integer
+let isloggedin= true //boolean
+
+//integer=> 2 to 2 power 53
+//bigint
+//string=>""
+// boolean => true,false
+// null=> stand alone value 
+// undefined =>
