a79e7a14d39983876bf7df61fc043c83a6e8dcf8
Parth0177 <parthtiwari670@gmail.com>
2025-02-28 17:37:51 +0000

    conversion of Datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = '33abc'
+
+console.log(typeof score);
+console.log(typeof(score)); //using object
+
+let valueInNumber = Number(score) //type conversion
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//'33'=>33
+// '33abc'=> NaN
+// 'true'=>1 ; false=>0
+
+let isLoggedIn = 1
+let booleanisLoggedIn= Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+// 1=> true
+//0=>false
+// "parth"=>true
+
+
+let someNumber=43
+let StringNumber= String(someNumber)
+console.log(StringNumber);
+
+console.log(typeof(StringNumber));
+
