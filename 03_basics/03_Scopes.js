244633bb30cc4572f0fd0a92991f7da67cd870b5
Parth0177 <parthtiwari670@gmail.com>
2025-03-04 07:43:10 +0000

    scopes in js

--- 03_basics/03_Scopes.js
+++ 03_basics/03_Scopes.js
@@ -7,7 +7,42 @@ console.log(a,b,c)
 if(true){
     let a=10
     const b=20    // Block Scope   
-    console.log("Inner:",a,b);
-    
+    console.log("Inner:",a,b);    
 }
 
+function one(){
+   const username="Parth"
+   
+   function two(){
+    const website="Youtube"
+    console.log(username);
+    
+   }
+   //console.log(website);
+    two()
+   
+}
+one()
+
+
+if(true){
+    const username="Parth"
+    if(username==="Parth"){
+        const website=" Youtube"
+        console.log(username + website);
+        
+    }
+}
+
+//++++++++++++++++ Intersesting +++++++++++++++
+addone(5)
+function addone(value){
+    return value+2
+}
+
+
+const addTwo=function(value){
+    return value+4
+}
+
+addTwo(6)
