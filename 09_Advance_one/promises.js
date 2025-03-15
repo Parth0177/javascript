c81e26e3ad5bde721785c366774d27e1d1a8f3cf
Parth Tiwari <parthtiwari670@gmail.com>
2025-03-15 15:03:15 +0000

    fetch in js

--- 09_Advance_one/promises.js
+++ 09_Advance_one/promises.js
@@ -120,12 +120,3 @@ cosumepromise5()
 getAllUsers()*/
 
 
-fetch('https://jsonplaceholder.typicode.com/users')
-.then((response)=>{
-    return response.json()
-})
-.then((data)=>{
-    console.log(data);
-    
-})
-.catch((error)=>console.log(error))
